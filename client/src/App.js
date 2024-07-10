b8ed65f04114a102446e882222561f1328e56eb4
Hemeswari-S <hemeswarisekar3@gmail.com>
2024-07-10 10:37:40 +0530

    bY heMES

--- client/src/App.js
+++ client/src/App.js
@@ -9,12 +9,16 @@ const rows = [
   { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
   { id: 3, col1: 'MUI', col2: 'is Amazing' },
 ];
+const columns = [
+  { field: 'col1', headerName: 'Column 1', width: 150 },
+  { field: 'col2', headerName: 'Column 2', width: 150 },
+];
 
 function App() {
   return (
     <div className="App">
       {/* <Root/> */}
-      <DataGrid rows={rows}/>
+      <DataGrid rows={rows} columns={columns}/>
     </div>
   );
 }
