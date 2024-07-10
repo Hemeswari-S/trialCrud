bafef222d69d1bd09d4a57849a41fee95033eb88
Hemeswari-S <hemeswarisekar3@gmail.com>
2024-07-10 10:33:58 +0530

    By Hemes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,20 @@
 
+import { DataGrid } from '@mui/x-data-grid';
 import Root from './Routes/Root';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
+const rows = [
+  { id: 1, col1: 'Hello', col2: 'World' },
+  { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
+  { id: 3, col1: 'MUI', col2: 'is Amazing' },
+];
 
 function App() {
   return (
     <div className="App">
-      <Root/>
+      {/* <Root/> */}
+      <DataGrid rows={rows}/>
     </div>
   );
 }
